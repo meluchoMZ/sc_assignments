c48bf4e0462859b977f12cd24b7ebf8fb312b3d4
meluchoMZ <miguelgodon@outlook.es>
2021-04-28 18:10:31 +0200

    Added commentaries

--- p2/hamming_code.m
+++ p2/hamming_code.m
@@ -9,37 +9,51 @@
 %% ERRORS: if the op parameter does not match any option, an error is raised.
 
 function [output_stream, n, k] = hamming_code(input_stream, operation_code, n)
+	% generates H (parity check matrix)
 	H = de2bi(1:n, 'right-msb')'; 
+	% generates G (generator matrix of the code for the parity check matrix H)
 	G = generatrix(H); 
+	% gets the size of G = equals rate of code
 	[k, n] = size(G);
 	if (strcmp(operation_code, 'encode'))
+		% distribution in rows of k elements
 		raw = reshape(input_stream, k, [])';
+		% matrix matrix multiplication to encode the input bits
 		encoded = mod(raw*G, 2);
+		% redistribution to output vector
 		output_stream = reshape(encoded', 1, []);
 		return;
 	end
 	if (strcmp(operation_code, 'decode'))
 		encoded = reshape(input_stream, n, [])';
+		% computes the sindrom of each n received bits
 		sindrom = mod(encoded*H', 2);
+		% computes error position on each received element
 		errors = bi2de(sindrom, 'right-msb');
 		error_indexes = []; ii = 1;
+		% saves the rows where sindrom != 0
 		while (ii <= length(errors))
 			if (errors(ii) ~= 0)
 				error_indexes = [error_indexes ii];
 			end
 			ii = ii+1;
 		end
+		% removes the null rows
 		errors(errors==0) = [];
-		%encoded(error_indexes, errors) = ~encoded(error_indexes, errors);
+		% flips the bits where errors are
 		for jj = 1:length(errors)
 			encoded(error_indexes(jj),errors(jj)) = ~encoded(error_indexes(jj),errors(jj));
 		end
+		% in decoding, must remove columns 2^n<n
 		a = []; ii = 0;
+		% computes wich columns must be removed
 		while (2^ii <= n)
 			a = [a 2^ii];
 			ii = ii + 1;
 		end
+		% non data bit are removed, thus remaining the data bits
 		encoded(:, a) = [];
+		% redistribution again to output vector
 		output_stream = reshape(encoded', 1, []);
 		return;
 	end
